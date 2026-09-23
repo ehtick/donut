972b157a1d62cc59eaf2cdc0b3988d89ce9aaeea
Greg Humphreys <ghumphreys@nvidia.com>
2026-09-23 17:07:31 -0400

    Use merged upstream NVRHI and keep assertion test failures noninteractive

--- tests/src/memory_stats.cpp
+++ tests/src/memory_stats.cpp
@@ -187,6 +187,9 @@ int main(int argc, char** argv)
     SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
 #ifdef _MSC_VER
     _set_error_mode(_OUT_TO_STDERR);
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
     _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
 #endif
